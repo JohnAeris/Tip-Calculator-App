609ed41f3087ad2376d23869483fe9f9a0476023
johnaeris <barrotjohnaeris@gmail.com>
2022-11-13 22:53:06 +0800

    Add split user interface

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,33 +2,36 @@ package com.example.tipcalculator
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tipcalculator.components.InputTextField
+import com.example.tipcalculator.components.IconButton
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -152,11 +155,22 @@ fun TopContent(
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun BodyContent() {
 
+    UserForm() { bill ->
+        Log.d("AMOUNT", "User's Input: $bill")
+    }
+
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun UserForm(
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit = {}
+) {
     var bill = remember {
         mutableStateOf("")
     }
@@ -165,15 +179,22 @@ fun BodyContent() {
         bill.value.trim().isNotEmpty()
     }
 
+    var split by remember {
+        mutableStateOf(0)
+    }
+
     val keyboardController = LocalSoftwareKeyboardController.current
     val context = LocalContext.current
 
-    Surface(modifier = Modifier
-        .fillMaxWidth(),
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
         shape = RoundedCornerShape(CornerSize(8.dp)),
         border = BorderStroke(1.dp, color = colorResource(R.color.teal_blue)) ) {
 
-        Column(modifier = Modifier.padding(10.dp)) {
+        Column(modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()) {
             //Text Field
 
             InputTextField(
@@ -186,29 +207,83 @@ fun BodyContent() {
                         Toast.makeText(context, "Input the bill", Toast.LENGTH_SHORT).show()
                         return@KeyboardActions}
                     else {
+                        onValueChange(bill.value.trim())
                         keyboardController?.hide()
                     }
                 }
-
             )
 
-            //Tip Row
+            if (isBillValid) {
 
+                Text(
+                    text = "SPLIT",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Light,
+                    modifier = Modifier.padding(start = 10.dp, end = 10.dp)
+                )
 
+                Spacer(modifier = Modifier.height(10.dp))
 
-            //Split Row
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(bottom = 10.dp)
+                ) {
+
+                    IconButton(
+                        iconImage = painterResource(R.drawable.minus_ic),
+                        onClick = {
+                            if (split > 0) {
+                                split--
+                            } else {
+                                split = 0
+                            }
+                        })
+
+                    Spacer(modifier = Modifier.width(20.dp))
+
+                    Row() {
+                        Image(
+                            painter = painterResource(R.drawable.user_ic),
+                            contentDescription = "Number of person",
+                            modifier = Modifier.size(28.dp),
+                        )
+
+                        Spacer(modifier = Modifier.width(5.dp))
+
+                        Text(
+                            text = "$split",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(20.dp))
+
+                    IconButton(
+                        iconImage = painterResource(R.drawable.add_ic),
+                        onClick = {
+                            split++
+                        }
+                    )
+                }
+
+            } else {
+
+            }
+
 
 
 
         }
-
     }
 }
 
 
 
 
-/*@Preview(showBackground = true)*/
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TipCalculatorTheme {
