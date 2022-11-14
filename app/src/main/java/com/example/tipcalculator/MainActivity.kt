db35d13de7f7724f9ff1c24fa91acd5610f7895e
johnaeris <barrotjohnaeris@gmail.com>
2022-11-14 19:29:44 +0800

    Modify user interface layout

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.tipcalculator
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -9,7 +8,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
@@ -21,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
@@ -32,8 +29,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tipcalculator.components.InputTextField
 import com.example.tipcalculator.components.IconButton
+import com.example.tipcalculator.logic.totalAmountPerPerson
 import com.example.tipcalculator.logic.totalTipCalculation
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
+import kotlin.math.absoluteValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,32 +52,158 @@ fun MyApp() {
             .padding(20.dp),
         verticalArrangement = Arrangement.spacedBy(10.dp)) {
 
-        TopContent()
         BodyContent()
 
     }
 }
 
-//@Preview
+@Preview
 @Composable
 fun TopContent(
-    bill: Double = 0.0,
+    bill: String = "0.0",
     tip: Double = 0.0,
-    split: Int = 0,
-    total: Double = 0.0
+    split: Int = 1,
+    individualAmount: Double = 0.0
 ) {
 
-    var billD = "%.2f".format(bill)
     var tipD = "%.2f".format(tip)
-    var totalD = "%.2f".format(total)
+    //var totalD = "%.2f".format(total)
+
+    val billA = if (bill.isNullOrEmpty()) {
+        0.0
+    } else {
+        bill.toDouble()
+    }
+
+    var total = billA + tip
 
     Surface(modifier = Modifier
         .fillMaxWidth()
-        .height(180.dp)
+        .height(230.dp)
         .padding(start = 10.dp, end = 10.dp)
         .clip(shape = CircleShape.copy(CornerSize(12.dp)))) {
 
-        Row(
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(
+                text = "TOTAL AMOUNT",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+            
+            Text(
+                text = "$ $total",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Divider(thickness = 1.dp)
+
+            Row(modifier = Modifier.padding(top = 10.dp)) {
+                Column(verticalArrangement = Arrangement.Center) {
+                    Text(
+                        text = "Bill",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = "Split",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = "Tip",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = "Per Person\n(Tip)",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(50.dp))
+
+                Column(verticalArrangement = Arrangement.spacedBy(5.dp)) {
+                    Text(
+                        text = ":",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Text(
+                        text = ":",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Text(
+                        text = ":",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Text(
+                        text = ":",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(50.dp))
+
+                Column(verticalArrangement = Arrangement.Center) {
+                    Text(
+                        text = "$$billA",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = "$split",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = "$$tipD",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = "$$individualAmount",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+                }
+            }
+
+
+
+
+        }
+
+        /*Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center,
             modifier = Modifier
@@ -86,78 +211,83 @@ fun TopContent(
         ) {
             Column(
                 horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.spacedBy(5.dp)
             ) {
                 Text(
-                    text = "Bill: ",
-                    fontSize = 20.sp,
+                    text = "Total",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                Text(
+                    text = "Bill",
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Light
                 )
 
-                Spacer(modifier = Modifier.height(10.dp))
-
                 Text(
-                    text = "Tip: ",
-                    fontSize = 20.sp,
+                    text = "Tip",
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Light
                 )
 
-                Spacer(modifier = Modifier.height(10.dp))
-
                 Text(
-                    text = "Split: ",
-                    fontSize = 20.sp,
+                    text = "Split",
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Light
                 )
 
-                Spacer(modifier = Modifier.height(20.dp))
+                Spacer(modifier = Modifier.height(5.dp))
 
                 Text(
-                    text = "Total: ",
-                    fontSize = 20.sp,
+                    text = "Per Person",
+                    fontSize = 18.sp,
                     fontWeight = FontWeight.SemiBold
                 )
             }
             
-            Spacer(modifier = Modifier.width(50.dp))
+            Spacer(modifier = Modifier.width(20.dp))
 
             Column(
-                horizontalAlignment = Alignment.Start
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.spacedBy(5.dp)
             ) {
                 Text(
-                    text = "$ $billD",
-                    fontSize = 20.sp,
+                    text = "$ $billA",
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Light
                 )
-
-                Spacer(modifier = Modifier.height(10.dp))
 
                 Text(
                     text = "$ $tipD",
-                    fontSize = 20.sp,
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Light
                 )
 
-                Spacer(modifier = Modifier.height(10.dp))
-
                 Text(
-                    text = "0",
-                    fontSize = 20.sp,
+                    text = "$split",
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Light
                 )
 
-                Spacer(modifier = Modifier.height(20.dp))
+                Spacer(modifier = Modifier.height(5.dp))
 
                 Text(
-                    text = "$ $totalD",
+                    text = "$ $individualAmount",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                Text(
+                    text = "$ $total",
                     fontSize = 20.sp,
                     fontWeight = FontWeight.SemiBold
                 )
             }
-        }
+        }*/
     }
 }
 
-@Preview
 @Composable
 fun BodyContent() {
 
@@ -174,7 +304,7 @@ fun UserForm(
     onValueChange: (String) -> Unit = {}
 ) {
     var bill = remember {
-        mutableStateOf("0.0")
+        mutableStateOf("")
     }
 
     val isBillValid = remember(bill.value) {
@@ -195,8 +325,21 @@ fun UserForm(
         mutableStateOf(0.0)
     }
 
+    var amountPerPerson by remember {
+        mutableStateOf(0.0)
+    }
+
+    var amountPerPersonD = "%.2f".format(amountPerPerson)
+
     val keyboardController = LocalSoftwareKeyboardController.current
     val context = LocalContext.current
+
+    TopContent(
+        bill = bill.value,
+        tip = tipAmount,
+        split = split,
+        individualAmount = amountPerPersonD.toDouble()
+    )
 
     Surface(
         modifier = Modifier
@@ -207,6 +350,7 @@ fun UserForm(
         Column(modifier = Modifier
             .padding(10.dp)
             .fillMaxWidth()) {
+
             //Text Field
 
             InputTextField(
@@ -219,13 +363,13 @@ fun UserForm(
                         Toast.makeText(context, "Input the bill", Toast.LENGTH_SHORT).show()
                         return@KeyboardActions}
                     else {
-                        onValueChange(bill.value.trim())
+                        onValueChange(bill.toString().trim())
                         keyboardController?.hide()
                     }
                 }
             )
 
-            if (true) {
+            if (isBillValid) {
 
                 //SPLIT
                 
@@ -250,10 +394,15 @@ fun UserForm(
                     IconButton(
                         iconImage = painterResource(R.drawable.minus_ic),
                         onClick = {
-                            if (split > 0) {
+                            if (split > 1) {
                                 split--
+                                amountPerPerson = totalAmountPerPerson(
+                                    totalBill = bill.value.toDouble(),
+                                    split = split.absoluteValue,
+                                    tipPercentage = tipPercentage
+                                )
                             } else {
-                                split = 0
+                                split = 1
                             }
                         })
 
@@ -281,6 +430,11 @@ fun UserForm(
                         iconImage = painterResource(R.drawable.add_ic),
                         onClick = {
                             split++
+                            amountPerPerson = totalAmountPerPerson(
+                                totalBill = bill.value.toDouble(),
+                                split = split.absoluteValue,
+                                tipPercentage = tipPercentage
+                            )
                         }
                     )
                 }
@@ -321,6 +475,11 @@ fun UserForm(
                         tipSlider = value
 
                         tipAmount = totalTipCalculation(bill.value.toDouble(), tipPercentage)
+                        amountPerPerson = totalAmountPerPerson(
+                            totalBill = bill.value.toDouble(),
+                            split = split.absoluteValue,
+                            tipPercentage = tipPercentage
+                        )
                     },
                     modifier = Modifier.padding(start = 10.dp, end = 10.dp)
                 )
