64fd6404a33c1a760d2b7a594f6fd729a0217885
johnaeris <barrotjohnaeris@gmail.com>
2022-11-14 21:15:02 +0800

    Add component rectangle button

--- app/src/main/java/com/example/tipcalculator/components/RectangleButton.kt
+++ app/src/main/java/com/example/tipcalculator/components/RectangleButton.kt
@@ -1,18 +1,25 @@
 package com.example.tipcalculator.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import com.example.tipcalculator.R
 
 @Composable
 fun RectangleButton(
@@ -23,16 +30,24 @@ fun RectangleButton(
 ) {
     Card(
         modifier = Modifier
+            .wrapContentSize()
             .fillMaxWidth()
             .clickable {
-                onClick
-            }
-            .wrapContentHeight()
+                onClick.invoke()
+            },
+        elevation = elevation,
+        shape = RoundedCornerShape(CornerSize(8.dp)),
+        border = BorderStroke(1.dp, color = colorResource(id = R.color.cool_black)),
+        backgroundColor = colorResource(id = R.color.teal_blue)
     ) {
         Text(
             text = label,
             fontSize = 16.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            maxLines = 1,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(10.dp),
+            color = Color.White
         )
     }
 }
